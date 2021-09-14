65a77c18ed68420c7197599173783d28dda8f001
MDz1985 <m_d@tut.by>
2021-09-14 19:33:20 +0300

    feat: char count done

--- src/st-common-character-count.js
+++ src/st-common-character-count.js
@@ -12,9 +12,16 @@ import { NotImplementedError } from '../extensions/index.js';
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
 export default function getCommonCharacterCount(s1, s2 ) {
-  s1 += s2;
+  let long = 0;
+  let d1 = s1;
+  let d2 = s2;
 
-  return s1.replace (/(\w)\1+/g, '').length
-  //throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ for (let char of s1){
+  if (d2.includes(char)&& d1.includes(char)){
+    long ++;
+    d1 = d1.replace(char, '');
+    d2 = d2.replace(char, '');}
+    
+ }
+ return long;
 }
