73e0a0e808cd0206b503621f1b671b18659c1b98
MDz1985 <m_d@tut.by>
2021-09-16 13:46:35 +0300

    feat: repeter done

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,29 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
+export default function repeater(str, options) {
+  if (str === null){str = 'null';}
+  str = String(str);
+  let repeatTimes = 1;
+  let separator = '+';
+  let addition = '';
+  let additionRepeatTimes = 1;
+  let additionSeparator = '|';
+  if (options.repeatTimes){repeatTimes = options.repeatTimes;}
+  if (options.separator){separator = options.separator;}
+  if (options.addition){addition = String(options.addition);}
+  if (typeof(options.addition) === 'boolean'){addition = String(options.addition);}
+  if (options.addition === null){addition = 'null';}
+  if (options.additionRepeatTimes){additionRepeatTimes = options.additionRepeatTimes;}
+  if (options.additionSeparator){additionSeparator = options.additionSeparator;}
+   let aSL = 0 - additionSeparator.length;
+   let aS = 0 - separator.length;
+  addition = ((addition+additionSeparator).repeat(additionRepeatTimes))
+  addition = addition.slice(0, aSL);
+  str = str + addition + separator;
+  str = (str.repeat(repeatTimes)).slice(0, aS)
+return str
+
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
